9f95de70f7e344a4687726e80046bcfafb075133
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-01-13 00:04:00 +0300

    Add point plane relations

--- geometry/relations3.py
+++ geometry/relations3.py
@@ -1,20 +1,41 @@
-from .vec3 import Vec3, add3, sub3, len3, project3
+from .vec3 import Vec3, add3, sub3, mul3, len3, project3, norm3
 from .line3 import Line3
+from .plane3 import Plane3
 
-def point_line_projection(point: Vec3, line: Line3) -> Vec3:
-    '''Projects point on line.
+def point_line_projection(target_point: Vec3, line: Line3) -> Vec3:
+    '''Projects point onto line.
 
-    For an arbitrary line point project vector from that point to target point onto line.
+    For an arbitrary line point project vector from line point to target point onto line.
     Offset line point by that projection.
     '''
-    anchor_to_point_dir = sub3(point, line.anchor)
-    anchor_to_point_proj = project3(anchor_to_point_dir, line.direction)
-    return add3(line.anchor, anchor_to_point_proj)
+    line_point_to_target_dir = sub3(target_point, line.anchor)
+    line_point_to_target_proj = project3(line_point_to_target_dir, line.direction)
+    return add3(line.anchor, line_point_to_target_proj)
 
-def point_line_distance(point: Vec3, line: Line3) -> float:
+def point_line_distance(target_point: Vec3, line: Line3) -> float:
     '''Finds distance from point to line.
 
     Take distance between target point and its projection onto line.
     '''
-    point_proj = point_line_projection(point, line)
-    return len3(sub3(point, point_proj))
+    target_point_proj = point_line_projection(target_point, line)
+    return len3(sub3(target_point, target_point_proj))
+
+def point_plane_projection(target_point: Vec3, plane: Plane3) -> Vec3:
+    '''Projects point onto plane.
+
+    For an arbitrary plane point project vector from target point to plane point onto plane normal.
+    Offset target point by that projection.
+    '''
+    # Set of plane points is defined by the equation: (normal, x) = distance.
+    plane_point = mul3(norm3(plane.normal), plane.distance)
+    target_to_plane_point_dir = sub3(plane_point, target_point)
+    target_to_plane_point_proj = project3(target_to_plane_point_dir, plane.normal)
+    return add3(target_point, target_to_plane_point_proj)
+
+def point_plane_distance(target_point: Vec3, plane: Plane3) -> float:
+    '''Finds distance from point to plane.
+
+    Take distance between target point and its projection onto plane.
+    '''
+    target_point_proj = point_plane_projection(target_point, plane)
+    return len3(sub3(target_point, target_point_proj))
--- tests/test_relations3.py
+++ tests/test_relations3.py
@@ -2,6 +2,7 @@ import unittest
 import geometry.relations3 as r3
 from geometry.vec3 import Vec3
 from geometry.line3 import Line3
+from geometry.plane3 import Plane3
 
 class TestRelations3(unittest.TestCase):
     def test_point_line_projection(self):
@@ -17,3 +18,17 @@ class TestRelations3(unittest.TestCase):
         self.assertAlmostEqual(r3.point_line_distance(Vec3(4, 3, 4), line), 0)
         self.assertAlmostEqual(r3.point_line_distance(Vec3(2, 0, 2), line), 3)
         self.assertAlmostEqual(r3.point_line_distance(Vec3(4, 3, 0), line), 2.82842712)
+
+    def test_point_plane_projection(self):
+        plane = Plane3(normal=Vec3(0, 5, 0), distance=4)
+        self.assertEqual(r3.point_plane_projection(Vec3(3, 4, 2), plane), Vec3(3, 4, 2))
+        self.assertEqual(r3.point_plane_projection(Vec3(0, 4, 9), plane), Vec3(0, 4, 9))
+        self.assertEqual(r3.point_plane_projection(Vec3(1, 0, 4), plane), Vec3(1, 4, 4))
+        self.assertEqual(r3.point_plane_projection(Vec3(2, 9, 1), plane), Vec3(2, 4, 1))
+
+    def test_point_plane_distance(self):
+        plane = Plane3(normal=Vec3(0, 5, 0), distance=4)
+        self.assertAlmostEqual(r3.point_plane_distance(Vec3(3, 4, 2), plane), 0)
+        self.assertAlmostEqual(r3.point_plane_distance(Vec3(0, 4, 9), plane), 0)
+        self.assertAlmostEqual(r3.point_plane_distance(Vec3(1, 0, 4), plane), 4)
+        self.assertAlmostEqual(r3.point_plane_distance(Vec3(2, 9, 1), plane), 5)
