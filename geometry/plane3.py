d1288de3b96f6ad42bd0ad630beed7386e378ecb
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-01-12 10:42:32 +0300

    Add plane

---
+++ geometry/plane3.py
@@ -0,0 +1,19 @@
+import typing
+from .vec3 import Vec3, XUNIT3, YUNIT3, ZUNIT3, eq3, _eq
+
+class Plane3(typing.NamedTuple):
+    normal: Vec3
+    distance: float
+
+    def __eq__(self, other: object) -> bool:
+        try:
+            return eq_plane3(self, typing.cast(Plane3, other))
+        except: # pylint: disable=bare-except
+            return False
+
+XOY3 = Plane3(normal=ZUNIT3, distance=0)
+YOZ3 = Plane3(normal=XUNIT3, distance=0)
+ZOX3 = Plane3(normal=YUNIT3, distance=0)
+
+def eq_plane3(a: Plane3, b: Plane3) -> bool:
+    return eq3(a.normal, b.normal) and _eq(a.distance, b.distance)
---
+++ tests/test_plane3.py
@@ -0,0 +1,19 @@
+import unittest
+# pylint: disable=W0401,W0614
+from geometry.plane3 import *
+
+class TestPlane33(unittest.TestCase):
+    def test_constants(self):
+        self.assertEqual(XOY3, Plane3(normal=Vec3(0, 0, 1), distance=0))
+        self.assertEqual(YOZ3, Plane3(normal=Vec3(1, 0, 0), distance=0))
+        self.assertEqual(ZOX3, Plane3(normal=Vec3(0, 1, 0), distance=0))
+
+    def test_eq_plane33(self):
+        self.assertEqual(eq_plane3(
+            Plane3(normal=Vec3(1, 2, 3), distance=4),
+            Plane3(normal=Vec3(1, 2, 3), distance=4)
+        ), True)
+        self.assertEqual(eq_plane3(
+            Plane3(normal=Vec3(1, 2, 3), distance=4),
+            Plane3(normal=Vec3(2, 3, 1), distance=4)
+        ), False)
