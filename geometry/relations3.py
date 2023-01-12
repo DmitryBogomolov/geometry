8a90f9f98aacfc7c57433633bfbb9eee3870a190
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-01-12 23:39:21 +0300

    Add some doc

--- geometry/relations3.py
+++ geometry/relations3.py
@@ -1,11 +1,20 @@
 from .vec3 import Vec3, add3, sub3, len3, project3
 from .line3 import Line3
 
-def project_point_line(point: Vec3, line: Line3) -> Vec3:
+def point_line_projection(point: Vec3, line: Line3) -> Vec3:
+    '''Projects point on line.
+
+    For an arbitrary line point project vector from that point to target point onto line.
+    Offset line point by that projection.
+    '''
     anchor_to_point_dir = sub3(point, line.anchor)
     anchor_to_point_proj = project3(anchor_to_point_dir, line.direction)
     return add3(line.anchor, anchor_to_point_proj)
 
 def point_line_distance(point: Vec3, line: Line3) -> float:
-    point_proj = project_point_line(point, line)
+    '''Finds distance from point to line.
+
+    Take distance between target point and its projection onto line.
+    '''
+    point_proj = point_line_projection(point, line)
     return len3(sub3(point, point_proj))
--- tests/test_relations3.py
+++ tests/test_relations3.py
@@ -4,12 +4,12 @@ from geometry.vec3 import Vec3
 from geometry.line3 import Line3
 
 class TestRelations3(unittest.TestCase):
-    def test_project_point_line(self):
+    def test_point_line_projection(self):
         line = Line3(anchor=Vec3(0, 3, 0), direction=Vec3(2, 0, 2))
-        self.assertEqual(r3.project_point_line(Vec3(1, 3, 1), line), Vec3(1, 3, 1))
-        self.assertEqual(r3.project_point_line(Vec3(4, 3, 4), line), Vec3(4, 3, 4))
-        self.assertEqual(r3.project_point_line(Vec3(2, 0, 2), line), Vec3(2, 3, 2))
-        self.assertEqual(r3.project_point_line(Vec3(4, 3, 0), line), Vec3(2, 3, 2))
+        self.assertEqual(r3.point_line_projection(Vec3(1, 3, 1), line), Vec3(1, 3, 1))
+        self.assertEqual(r3.point_line_projection(Vec3(4, 3, 4), line), Vec3(4, 3, 4))
+        self.assertEqual(r3.point_line_projection(Vec3(2, 0, 2), line), Vec3(2, 3, 2))
+        self.assertEqual(r3.point_line_projection(Vec3(4, 3, 0), line), Vec3(2, 3, 2))
 
     def test_point_line_distance(self):
         line = Line3(anchor=Vec3(0, 3, 0), direction=Vec3(2, 0, 2))
